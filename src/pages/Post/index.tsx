bdb890bcf47b5e8e7d65fa2c0fae62d2231e1567
bkkater <biancamedeios.bs10@gmail.com>
2020-08-23 23:14:43 -0300

    [add] post page :) and FINISHEEEEEED

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,32 +1,102 @@
-import React from 'react';
-import { View, Image } from 'react-native';
-import { BorderlessButton } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, Image, Text, Switch } from 'react-native';
+import { BorderlessButton, TextInput, ScrollView, RectButton } from 'react-native-gesture-handler';
 
 import style from './styles'
 import Header from '../../components/Header';
-import goBack from '../../assets/goBack.png'
 import cake from '../../assets/cake.png'
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
 
-
-function Post() {
-  const { navigate } = useNavigation();
-
-  function handleNavigateToBack() {
-    navigate('MenuTabs')
-  }
+const Post = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
   return (
     <View>
-      <Header title='Nova Postagem' icon={false}>
-        <BorderlessButton onPress={handleNavigateToBack}>
-          <Image source={goBack} style={style.image}></Image>
-        </BorderlessButton>
-      </Header>
+      <ScrollView>
+        <Header title='Nova Postagem' icon={false}></Header>
 
-      <View style={style.imageBlock}>
-        <Image source={cake} />
-      </View>
+
+
+        <View style={style.imageBlock}>
+          <Image source={cake} style={style.cake} />
+        </View>
+
+        <View style={style.fieldGroup}>
+          <TextInput
+            style={style.productName}
+            placeholder='Nome do produto'
+          />
+          <TextInput
+            style={style.productPrice}
+            placeholder='Preço'
+          />
+        </View>
+
+        <View style={style.section}>
+          <Text style={style.sectionTitle}>Cashback</Text>
+
+          <View style={style.sectionCashback}>
+            <Text style={style.cashbackPercent}>1%</Text>
+
+            <View style={style.percentInput}>
+              <BorderlessButton><Text style={style.difInputText}>-</Text></BorderlessButton>
+              <BorderlessButton><Text style={style.inputText}>+</Text></BorderlessButton>
+            </View>
+          </View>
+        </View>
+
+        <View style={style.section}>
+          <Text style={style.sectionTitle}>Descrição</Text>
+
+          <Text style={style.descriptionInput}>Compre através do aplicativo Ame e receba 20% de cashback.
+          link.perfil/ame  @useamedigital #Ame #Cashback #Amepequenonegocio</Text>
+        </View>
+
+        <View style={style.hr}></View>
+
+        <View style={style.section}>
+          <Text style={style.sectionTitle}>Publicar também em:</Text>
+
+          <View style={style.option}>
+            <View style={style.optionDescription}>
+              <Image source={facebook}></Image>
+              <Text style={style.optionDescriptionText}>Facebook</Text>
+            </View>
+            <Switch
+              trackColor={{ false: "#CECECE", true: "#FF2D55" }}
+              thumbColor={"#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={!isEnabled}
+            />
+          </View>
+
+          <View style={style.option}>
+            <View style={style.optionDescription}>
+              <Image source={instagram}></Image>
+              <Text style={style.optionDescriptionText}>Instagram</Text>
+            </View>
+            <Switch
+              trackColor={{ false: "#CECECE", true: "#FF2D55" }}
+              thumbColor={"#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
+
+        </View>
+
+        <RectButton style={style.button}>
+          <Text style={style.buttonText}>Postar</Text>
+        </RectButton>
+      </ScrollView>
+
+
+
+
     </View>
   )
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -8,7 +8,110 @@ const styles = StyleSheet.create({
     imageBlock: {
         height: 184,
         backgroundColor: '#fafafa'
-    }
+    },
+    cake: {
+        marginTop: -1
+    },
+    fieldGroup: {
+        flexDirection: 'row',
+        margin: 20
+    },
+    productName: {
+        width: 200,
+        height: 44,
+        backgroundColor: '#F9F9F9',
+        borderRadius: 12,
+        padding: 10
+    },
+    productPrice: {
+        flex: 1,
+        height: 44,
+        backgroundColor: '#F9F9F9',
+        borderRadius: 8,
+        padding: 10,
+        marginLeft: 10
+    },
+    section: {
+        margin: 20,
+        marginTop: 10
+    },
+    sectionTitle: {
+        fontFamily: 'Ubuntu_500Medium',
+        fontSize: 18
+    },
+    sectionCashback: {
+        flexDirection: 'row',
+        position: "absolute",
+        bottom: -40,
+        right: 0,
+        marginRight: 20,
+        alignItems: "center"
+    },
+    cashbackPercent: {
+        marginRight: 20,
+        fontFamily: 'Ubuntu_400Regular',
+        fontSize: 18,
+        color: '#FF2D55'
+    },
+    percentInput: {
+        flexDirection: 'row',
+        padding: 10,
+        backgroundColor: '#E9E9E9',
+        justifyContent: 'space-around',
+        width: 115,
+        borderRadius: 8
+    },
+    inputText: {
+        fontFamily: 'Ubuntu_500Medium',
+        fontSize: 18,
+        color: '#424242'
+    },
+    difInputText: {
+        fontFamily: 'Ubuntu_500Medium',
+        fontSize: 18,
+        color: '#C4C4C4'
+    },
+    descriptionInput: {
+        marginTop: 10,
+        width: 343,
+        height: 118,
+        backgroundColor: '#F7F7F7',
+        borderRadius: 8,
+        padding: 18,
+        lineHeight: 20,
+        fontFamily: 'Ubuntu_400Regular',
+        fontSize: 15,
+    },
+    hr: {
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#949494',
+    },
+    option: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: "center",
+        marginTop: 5,
+        marginBottom: 10
+    },
+    optionDescription: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    optionDescriptionText: {
+        marginLeft: 10
+    },
+    button: {
+        backgroundColor: '#0091FF',
+        padding: 18,
+        margin: 80,
+        marginTop: 10,
+        alignItems: "center",
+        borderRadius: 8
+    },
+    buttonText: {
+        color: '#FFF',
+        fontFamily: 'Ubuntu_400Regular'
+    },
 })
 
 export default styles;
